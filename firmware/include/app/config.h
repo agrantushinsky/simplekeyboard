8e07ff2b27c0c131dfb03d26fd568a61a5313ee0
agrantushinsky <97311487+agrantushinsky@users.noreply.github.com>
2026-07-29 18:58:27 -0400

    Keyboard configuration

---
+++ firmware/include/app/config.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "drivers/mcp230xx.h"
+#include "stm32f0xx.h"
+
+#define MCP23017_ADDR 0x21
+#define MCP23008_ADDR 0x20
+
+typedef struct {
+    // I2C Peripherals
+    mcp230xx_t col_expander;
+    mcp230xx_t row_expander;
+
+    // Direct MCU Pins
+    struct {
+        GPIO_TypeDef* port;
+        uint32_t      pin;
+    } status_led;
+
+    struct {
+        GPIO_TypeDef* port;
+        uint32_t      pin;
+    } row_int_pin;
+} config_t;
+
+extern const config_t g_config;
+
+#endif
+
---
+++ firmware/src/app/config.c
@@ -0,0 +1,22 @@
+#include "app/config.h"
+#include "stm32f0xx_ll_gpio.h"
+
+const config_t g_config = {
+    .col_expander = { 
+        .i2c_addr = 0x21, 
+        .type = MCP_TYPE_23017 
+    },
+    .row_expander = { 
+        .i2c_addr = 0x20, 
+        .type = MCP_TYPE_23008 
+    },
+    .status_led = { 
+        .port = GPIOA,
+        .pin = LL_GPIO_PIN_6
+    },
+    .row_int_pin = { 
+        .port = GPIOA,
+        .pin = LL_GPIO_PIN_0
+    }
+};
+
